d6e965bf4b1626bffa2e4c5fa5b741e71d8f8e34
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-03-01 15:42:40 +0700

    update dropdown

---
+++ lib/Result.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  const Result({
+    Key key,
+    @required double kelvin,
+    @required double reamur,
+  }) : _kelvin = kelvin, _reamur = reamur, super(key: key);
+
+  final double _kelvin;
+  final double _reamur;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row (
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget> [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Text('Suhu dalam Kelvin',
+              style: TextStyle(height: 1.5)
+              ),
+              Text('$_kelvin', style: TextStyle(height: 1.5, fontSize:25),),
+            ],
+          ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Text('Suhu dalam Reamur',
+              style: TextStyle(height: 1.5)
+              ),
+              Text('$_reamur', style: TextStyle(height: 1.5, fontSize:25),),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
